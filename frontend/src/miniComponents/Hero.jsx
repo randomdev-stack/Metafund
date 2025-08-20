5ac95bd0dcff671b6ee7e328a4d82e6d32280a0c
saud <saudbaig2007@gmail.com>
2025-08-20 13:09:52 -0700

    commit

---
+++ frontend/src/miniComponents/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="banner">
+        <h1>Volunteers</h1>
+        <h3>Needed</h3>
+        <p>
+          Donations with Encryption
+        </p>
+        <Link to={"/donate"} className="btn">
+          Donate Now
+        </Link>
+      </div>
+      <div className="banner">
+        <img src="/hero.png" alt="hero" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
